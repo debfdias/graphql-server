d5089803866abd80cb749d7c260507a7fbed1dca
debfdias <debfdias@gmail.com>
2023-03-05 10:01:29 -0300

    Initial commit

---
+++ api/index.js
@@ -0,0 +1,27 @@
+import { ApolloServer, gql } from "apollo-server-express";
+import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
+import http from "http";
+import express from "express";
+import cors from "cors";
+import { typeDefs } from "../graphql/typeDefs";
+import { resolvers } from "../graphql/resolvers";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+const httpServer = http.createServer(app);
+
+const startApolloServer = async (app, httpServer) => {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+  server.applyMiddleware({ app });
+};
+
+startApolloServer(app, httpServer);
+
+export default httpServer;
